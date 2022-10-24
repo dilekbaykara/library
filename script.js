2500fd06afb47c86b1bde7b43aaf1c20898cac29
Dilek Baykara <baykara.d@gmail.com>
2022-10-24 14:54:54 -0400

    "added pop up form and eventlistener for button"

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function theGodfather.info(name,title,) {
-    
+/* let myLibrary = [];
+function Book() {
+  // the constructor...
 }
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function newBook(title,author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+newBook.prototype.info = function() {
+  console.log()
+} */
+
+
+const popUpForm = document.querySelector(".form-popup");
+const button = document.querySelector("#addBook");
+
+button.addEventListener("click", () => {
+  popUpForm.style.display = "block";
+});
