617345772166fe1d836b0d50d565eee73fac8a79
Dilek Baykara <baykara.d@gmail.com>
2022-10-29 08:25:24 -0400

    adjusted pop up div to keep content from spilling out

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ const library = new Library()
 const popUpForm = document.querySelector(".form-popup");
 const button = document.querySelector("#addBook");
 const overlay = document.getElementById('overlay');
+const booksGrid = document.getElementById('booksGrid');
+const addBookForm = document.getElementById('addBookForm');
 
 // Form Pop Up function //
 document.getElementById('invisibleDiv').onclick = function()
@@ -56,3 +58,57 @@ button.addEventListener("click", () => {
   popUpForm.style.display = "block";
   overlay.style.display = "block";
 });
+
+const updateBooksGrid = () => {
+  resetBooksGrid()
+  for (let book of library.books) {
+    createBookCard(book)
+  }
+}
+
+// Card for grid function //
+const createBookCard = (book) => {
+  const bookCard = document.createElement('div')
+  const title = document.createElement('p')
+  const author = document.createElement('p')
+  const pages = document.createElement('p')
+  const buttonGroup = document.createElement('div')
+  const readBtn = document.createElement('button')
+  const removeBtn = document.createElement('button')
+
+  bookCard.classList.add('book-card')
+  buttonGroup.classList.add('button-group')
+  readBtn.classList.add('btn')
+  removeBtn.classList.add('btn')
+  readBtn.onclick = toggleRead
+  removeBtn.onclick = removeBook
+
+  title.textContent = `"${book.title}"`
+  author.textContent = book.author
+  pages.textContent = `${book.pages} pages`
+  removeBtn.textContent = 'Remove'
+
+  if (book.isRead) {
+    readBtn.textContent = 'Read'
+    readBtn.classList.add('btn-light-green')
+  } else {
+    readBtn.textContent = 'Not read'
+    readBtn.classList.add('btn-light-red')
+  }
+
+  bookCard.appendChild(title)
+  bookCard.appendChild(author)
+  bookCard.appendChild(pages)
+  buttonGroup.appendChild(readBtn)
+  buttonGroup.appendChild(removeBtn)
+  bookCard.appendChild(buttonGroup)
+  booksGrid.appendChild(bookCard)
+}
+
+const getBookFromInput = () => {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const isRead = document.getElementById('isRead').checked
+  return new Book(title, author, pages, isRead)
+}
