bc456aaa270ec8f31a590c68d8c8c35b38daacc8
Dilek Baykara <baykara.d@gmail.com>
2022-11-04 12:46:31 -0400

    Added eventListener to submit button

--- script.js
+++ script.js
@@ -1,88 +1,96 @@
 // Library Functions //
-
-let myLibrary = [];
-
 class Book {
-constructor(title, author, pages, read) {
-    this.title = form.title.value;
-    this.author = form.author.value;
-    this.pages = form.pages.value + 'pages';
-    this.read = form.read.checked;
-}
-}
-
-function addBookToLibrary(){
-
-}
-
-
-// User interface //
-const popUpForm = document.querySelector(".form-popup");
-const button = document.querySelector("#addBook");
-const overlay = document.getElementById('overlay');
-const booksGrid = document.getElementById('booksGrid');
-const addBookForm = document.getElementById('addBookForm');
-const bookCard = document.querySelector('.book-card');
-
-// Form Pop Up function //
-document.getElementById('invisibleDiv').onclick = function()
-{
-   popUpForm.style.display = "none"; 
-   overlay.style.display = "none";
-};
-
-button.addEventListener("click", () => {
-  popUpForm.style.display = "block";
-  overlay.style.display = "block";
-});
-
-const updateBooksGrid = () => {
-  resetBooksGrid()
-  for (let book of library.books) {
-    createBookCard(book)
+  constructor(title, author, pages, read) {
+      this.title = form.title.value;
+      this.author = form.author.value;
+      this.pages = form.pages.value + 'pages';
+      this.read = form.read.checked;
   }
-}
-
-// Card for grid function //
-const createBookCard = (book) => {
-  const bookCard = document.createElement('div')
-  const title = document.createElement('p')
-  const author = document.createElement('p')
-  const pages = document.createElement('p')
-  const buttonGroup = document.createElement('div')
-  const readBtn = document.createElement('button')
-  const removeBtn = document.createElement('button')
-
-  title.textContent = `"${book.title}"`
-  author.textContent = book.author
-  pages.textContent = `${book.pages} pages`
-  removeBtn.textContent = 'Remove'
-
-  if (book.isRead) {
-    readBtn.textContent = 'Read'
-    readBtn.classList.add('submit');
-  } else {
-    readBtn.textContent = 'Not read'
-    readBtn.classList.add('btn-light-red')
   }
-
-  bookCard.appendChild(title)
-  bookCard.appendChild(author)
-  bookCard.appendChild(pages)
-  buttonGroup.appendChild(readBtn)
-  buttonGroup.appendChild(removeBtn)
-  bookCard.appendChild(buttonGroup)
-  booksGrid.appendChild(bookCard)
-}
-
-const getBookFromInput = () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const isRead = document.getElementById('isRead').checked
-  return new Book(title, author, pages, isRead)
-}
-
-
-const addBtn = document.querySelector("#submit");
-addBtn.addEventListener('click', createBookCard);
+  
+  //creates book from Book Constructor, adds to library
+  let myLibrary = [];
+  let newBook;
+  
+  function addBookToLibrary() {
+      event.preventDefault();
+      popUpForm.style.display = 'none';
+      newBook = new Book(title, author, pages); 
+      myLibrary.push(newBook); 
+      render(); 
+      form.reset();
+  }
+  
+  //Creates book visual in browser
+  function render() {
+      const books = document.querySelectorAll('.book');
+      books.forEach(book => display.removeChild(book));
+     
+      for (let i=0; i<myLibrary.length; i++){
+          createBook(myLibrary[i]);
+      }
+  }
+  
+  //
+  function createBook(item) {
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    
+    
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+  
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+  
+    authDiv.textContent = item.author;
+    authDiv.classList.add('author');
+    bookDiv.appendChild(authDiv);
+  
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
+  
+  bookCard.style.display = "block"; 
+  
+  };
+  
+  // User interface //
+  const popUpForm = document.querySelector('.form-popup');
+  const button = document.getElementById('addBook');
+  const overlay = document.getElementById('overlay');
+  const booksGrid = document.getElementById('booksGrid');
+  const bookCard = document.querySelector('.library-container');
+  const form = document.querySelector('.form-container');
+  const submitBtn = document.getElementById('submit');
+  
+  // Form Pop Up function //
+  document.getElementById('invisibleDiv').onclick = function()
+  {
+     popUpForm.style.display = "none"; 
+     overlay.style.display = "none";
+  };
+  
+  button.addEventListener("click", () => {
+    popUpForm.style.display = "block";
+    overlay.style.display = "block";
+  });
+  
+  const updateBooksGrid = () => {
+    resetBooksGrid()
+    for (let book of library.books) {
+      createBook(book)
+    }
+  };
+  
+  
+  // Card for grid function //
+  
+  submitBtn.addEventListener("click", (createBook) => { 
+    bookCard.style.display = "block";
+    popUpForm.style.display = "none";
+    overlay.style.display = "none";
+  });
