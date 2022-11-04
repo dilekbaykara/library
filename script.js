80b0a6c3cc507c797c6c8cbce445f4930683d714
Dilek Baykara <baykara.d@gmail.com>
2022-11-04 15:41:42 -0400

    added function to display input

--- script.js
+++ script.js
@@ -1,69 +1,52 @@
-// Library Functions //
-class Book {
+// Book Class : Represents a Book //
+class book {
   constructor(title, author, pages, read) {
-      this.title = form.title.value;
-      this.author = form.author.value;
-      this.pages = form.pages.value + 'pages';
-      this.read = form.read.checked;
+      this.title = title;
+      this.author = author;
+      this.pages = pages + 'pages';
+      this.read = read;
   }
   }
   
   //creates book from Book Constructor, adds to library
-  let myLibrary = [];
-  let newBook;
-  
-  function addBookToLibrary() {
-      event.preventDefault();
-      popUpForm.style.display = 'none';
-      newBook = new Book(title, author, pages); 
-      myLibrary.push(newBook); 
-      render(); 
-      form.reset();
+  class UI {
+    static displayBooks(){
+      const StoredBooks = [
+        {
+          title: 'Book One',
+          author: 'John Doe',
+        }
+      ];
+      const books = StoredBooks;
+      books.forEach((book) => UI.addBookToLibrary(book));
+      bookCard.style.display = "block";
+      popUpForm.style.display = "none";
+      overlay.style.display = "none";
+    }
+    static addBookToLibrary(book) {
+      const bookCard = document.querySelector('#library-container');
+      bookCard.innerHtml =
+`
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+`
+      bookCard.appendChild(title);
+      bookCard.appendChild(author);
+      bookCard.appendChild(pages);
+    }
   }
   
-  //Creates book visual in browser
-  function render() {
-      const books = document.querySelectorAll('.book');
-      books.forEach(book => display.removeChild(book));
-     
-      for (let i=0; i<myLibrary.length; i++){
-          createBook(myLibrary[i]);
-      }
-  }
+ 
   
-  //
-  function createBook(item) {
-    const bookDiv = document.createElement('div');
-    const titleDiv = document.createElement('div');
-    const authDiv = document.createElement('div');
-    const pageDiv = document.createElement('div');
-    
-    
-    bookDiv.classList.add('book');
-    bookDiv.setAttribute('id', myLibrary.indexOf(item));
-  
-    titleDiv.textContent = item.title;
-    titleDiv.classList.add('title');
-    bookDiv.appendChild(titleDiv);
-  
-    authDiv.textContent = item.author;
-    authDiv.classList.add('author');
-    bookDiv.appendChild(authDiv);
-  
-    pageDiv.textContent = item.pages;
-    pageDiv.classList.add('pages');
-    bookDiv.appendChild(pageDiv);
-  
-  bookCard.style.display = "block"; 
-  
-  };
-  
+
   // User interface //
+  const bookCard = document.querySelector('#library-container');
   const popUpForm = document.querySelector('.form-popup');
   const button = document.getElementById('addBook');
   const overlay = document.getElementById('overlay');
   const booksGrid = document.getElementById('booksGrid');
-  const bookCard = document.querySelector('.library-container');
   const form = document.querySelector('.form-container');
   const submitBtn = document.getElementById('submit');
   
@@ -79,18 +62,7 @@ class Book {
     overlay.style.display = "block";
   });
   
-  const updateBooksGrid = () => {
-    resetBooksGrid()
-    for (let book of library.books) {
-      createBook(book)
-    }
-  };
   
+  // Submit Button Event Listener (displays bookCard) //
   
-  // Card for grid function //
-  
-  submitBtn.addEventListener("click", (createBook) => { 
-    bookCard.style.display = "block";
-    popUpForm.style.display = "none";
-    overlay.style.display = "none";
-  });
+  submitBtn.addEventListener("click", UI.displayBooks); 
